9a551574f092c8616a3ed2724f5fa9759ec6344a
Manish Khedekar <mkhedekar@equalexperts.com>
2020-02-07 14:07:59 +0000

    Docs removed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ZettaByteFrontEnd
+# angular-2-front-end-test
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ZettaByteFrontEnd!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-2-Front-End-Test!');
   }));
 });
